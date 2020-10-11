ec3dad8488af8e547b3bc58eb6f8b599d6623c1a
Milena Wiśniewska <milena.wisniewska@britenet.com.pl>
2020-10-11 17:52:24 +0200

    first commit

---
+++ src/main/java/HandlingFramesTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class HandlingFramesTest {
+
+
+
+
+    @Test
+    public void handlingFramesTest() {
+
+    }
+
+}
---
+++ src/main/java/Tests/BaseSeleniumTest.java
@@ -0,0 +1,29 @@
+package Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseSeleniumTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        System.out.println("Starting the Test");
+        String driverPath = "C:\\Users\\BRITENET\\Downloads\\gs-accessing-data-mysql-master\\HealthServiceTests\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost:8080/");
+
+    }
+
+    @AfterMethod
+    public void teardown() throws InterruptedException {
+        System.out.println("Ending the Test");
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
+///comment
